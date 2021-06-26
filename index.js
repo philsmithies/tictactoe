b9325e609540cc81f75f4ca4dde243ccc22b50c3
Phil Smithies <philsmithies@gmail.com>
2021-06-26 11:42:24 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const gameBoard = (function(){
+  const gameBoard = []
+  const newGame = "wtf"
+  return {
+    gameBoard: gameBoard
+  }
+})();
+
+const Player = (name) => {
+  const getName  = () => name;
+
+  return {getName}
+}
+
